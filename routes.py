d7e5068990db3a319fa2b73b6fd4ea86be78d36c
tithiradia <tithi.radia@gmail.com>
2025-07-29 13:15:26 +0530

    Authetication added

--- routes.py
+++ routes.py
@@ -1,10 +1,15 @@
 from flask import render_template, request, redirect, url_for, session, jsonify, send_file, after_this_request
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
 from models import User
-from app import app, db
+from app import app, db, login_manager
 import os
 import random
 from gtts import gTTS
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
@@ -51,7 +56,11 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if user and check_password_hash(user.password, password):
-            session['user'] = user.username
+            login_user(user)
+            # If user was redirected to login from game route, send them back there
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
             return redirect(url_for('language_selection'))
         return "Invalid credentials."
     return render_template('login.html')
@@ -101,6 +110,11 @@ def generate_audio():
         return str(e), 500
 
 @app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    session.clear()
+    return redirect(url_for('index'))
 def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
